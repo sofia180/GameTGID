5ae7d31c328c466e321dd2d4d94b3599b9cde776
user <user@MacBook-Air-user.local>
2026-03-02 19:49:12 +0100

    fix: provide default TON Connect manifest for dev

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+const manifestUrl =
+  import.meta.env.VITE_TON_MANIFEST_URL ||
+  'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl={import.meta.env.VITE_TON_MANIFEST_URL}>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
